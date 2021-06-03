0caccaccb5722fef5e9bb0d6458ec22ca5426bd7
marstonlm <marston.luann@gmail.com>
2021-06-03 12:10:52 -0700

    Massive commit

---
+++ includes/footer.php
@@ -0,0 +1,49 @@
+
+  <!-- START Footer -->
+  <footer>
+    <div class="container">
+      <div class="footer-logo"><a href="index.php"><img src="images/site_logo.jpg" alt="A4 Loop logo" class="logo responsive"></a>
+        </div>
+      <!-- place social media icons here -->
+      <div class="social row">
+        <div class="column"><i class="fab fa-facebook-square"></i></div>
+        <div class="column"><i class="fab fa-instagram"></i></div>
+        <div class="column"><i class="fab fa-linkedin"></i></div>
+        <div class="column"><i class="fab fa-twitter"></i></div>
+      </div>
+    </div>
+    <div class="container footer-lower">
+      <div class="site-map">
+        <table>
+          <tr> 
+            <th><a href="services.php">Services</a></th>
+            <th><a href="portfolio.php">Portfolio</a></th>
+            <th><a href="about.php">About</a></th>
+            <th><a href="blog.php">Blog</a></th>
+            <th><a href="contact.php" class="th-contact">Contact</a></th>
+          </tr>
+    </table>
+  </div>
+  <div class="copyright"><p><small>&copy; <script>document.write(new Date().getFullYear())</script> by <a href="contactme.php" target="_blank">A4 Loop</a>, All Rights Reserved ~ <a href="disclaimer.php">Disclaimer</a> ~ <a href="http://validator.w3.org/check/referer" target="_blank">Valid HTML</a> ~ <a href="http://jigsaw.w3.org/css-validator/check?uri=referer" target="_blank">Valid CSS</a></small></p>
+  </div>
+</div>
+</footer>
+<!-- END Footer --> 
+
+</main>
+<!-- END WRAPPER -->
+
+<!-- JavaScript associated with the W3Schools.com Top Navigation Response Exercise --> 
+<script>
+  /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+  function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+</script>
+</body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,48 @@
+<?php include 'final-config.php'?>
+<!DOCTYPE html>
+<html lang="en">
+<head> 
+  <title><?=$title?></title>
+  <meta name="viewport" content="width=device-width" />
+  <meta name="robots" content="noindex,nofollow" />
+  <meta charset="utf-8">
+  <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+  <script src="https://s3.amazonaws.com/menumaker/menumaker.min.js" type="text/javascript"></script>
+  <script src="js/script.js"></script>
+  <!--  <link rel="stylesheet" href="css/nav.css" /> -->
+  <link rel="stylesheet" href="css/new-nav.css" />
+  <link rel="stylesheet" href="css/final.css" />
+  <link rel="stylesheet" href="css/form.css" />
+  <script src="https://kit.fontawesome.com/2358758f1c.js" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" />
+</head>
+
+<body>
+    <header>
+      <div class="header-logo"><a href="index.php"><img src="images/site_logo.jpg" class="logo responsive"></a>
+        <p>Software Your Way</p>
+      </div>
+      <nav id="cssmenu">
+      <ul>
+        <li><a class="selected" href="index.php"></a></li>
+        <li><a href="services.php"><span>Services</span></a>
+          <ul>
+            <li><a href="software.php"><span>Software</span></a></li>
+            <li><a href="mobile.php"><span>Mobile Apps</span></a></li>
+            <li><a href="database.php"><span>Database </span></a></li>
+          </ul></li>
+          <li><a href="portfolio.php"><span>Portfolio</span></a></li>
+          <li><a href="about.php"><span>About</span></a>
+            <ul>
+              <li><a href="ourstory.php"><span>Our Story</span></a></li>
+              <li><a href="team.php"><span>The Team</span></a></li>
+              <li><a href="career.php"><span>Career Oppertunities</span></a></li>
+            </ul>
+          </li>
+          <li><a href="blog.php"><span>Blog</span></a></li>
+          <li><a href="contact.php"><span>Contact</span></a></li>
+        </ul>
+      </nav>
+    </header>
+    <main>
+
