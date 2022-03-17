e925dd4311462e7c9f2cd4ac0d348ab3eab9f101
marstonlm <marston.luann@gmail.com>
2022-03-17 10:50:07 -0700

    March2022

--- includes/header.php
+++ includes/header.php
@@ -1,48 +1,77 @@
-<?php include 'final-config.php'?>
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head> 
-  <title><?=$title?></title>
-  <meta name="viewport" content="width=device-width" />
-  <meta name="robots" content="noindex,nofollow" />
+
+<head>
+  <!-- Required meta tags -->
   <meta charset="utf-8">
-  <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
-  <script src="https://s3.amazonaws.com/menumaker/menumaker.min.js" type="text/javascript"></script>
-  <script src="js/script.js"></script>
-  <!--  <link rel="stylesheet" href="css/nav.css" /> -->
-  <link rel="stylesheet" href="css/new-nav.css" />
-  <link rel="stylesheet" href="css/final.css" />
-  <link rel="stylesheet" href="css/form.css" />
-  <script src="https://kit.fontawesome.com/2358758f1c.js" crossorigin="anonymous"></script>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+
+  <!-- MAIN CSS -->
+  <link rel="stylesheet" href="css/styles.css" />
+  <link rel="stylesheet" href="css/nav.css" />
+  <script src="https://kit.fontawesome.com/902b4b52d9.js" crossorigin="anonymous"></script>
+
+
+  <title>A4Loop</title>
 </head>
 
 <body>
-    <header>
-      <div class="header-logo"><a href="index.php"><img src="images/site_logo.jpg" class="logo responsive"></a>
-        <p>Software Your Way</p>
+  <header class="px-5 py-3">
+    <div id="logoBox">
+      <div id="headerImgBox">
+        <a class="header-logo" href="#"><img class="header-logo" src="images/logo.jpg" alt="A4Loop Logo"></a>
       </div>
-      <nav id="cssmenu">
-      <ul>
-        <li><a class="selected" href="index.php"></a></li>
-        <li><a href="services.php"><span>Services</span></a>
-          <ul>
-            <li><a href="software.php"><span>Software</span></a></li>
-            <li><a href="mobile.php"><span>Mobile Apps</span></a></li>
-            <li><a href="database.php"><span>Database </span></a></li>
-          </ul></li>
-          <li><a href="portfolio.php"><span>Portfolio</span></a></li>
-          <li><a href="about.php"><span>About</span></a>
-            <ul>
-              <li><a href="ourstory.php"><span>Our Story</span></a></li>
-              <li><a href="team.php"><span>The Team</span></a></li>
-              <li><a href="career.php"><span>Career Oppertunities</span></a></li>
-            </ul>
-          </li>
-          <li><a href="blog.php"><span>Blog</span></a></li>
-          <li><a href="contact.php"><span>Contact</span></a></li>
-        </ul>
-      </nav>
-    </header>
-    <main>
+    </div>
+    <!-- START NAV-->
 
+    <nav class="navbar navbar-expand-lg">
+      <button class="navbar-toggler custom-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+    
+      <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home<span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Services
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+              <a class="dropdown-item" href="#">Database</a>
+              <a class="dropdown-item" href="#">Web Applications</a>
+              <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Portfolio</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Blog</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              About Us
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+              <a class="dropdown-item" href="#">Action</a>
+              <a class="dropdown-item" href="#">Another action</a>
+              <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <!-- END NAV -->
+  </header>
+
+  <!-- START MAIN and CONTAINER-->
+  <main class="container">
